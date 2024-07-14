24a8c31af7229b386d2b96ae48b0a337829d17e0
RfliFhri <raflifachriansyah43504@gmail.com>
2024-07-14 17:35:12 +0700

    menambahkan prettier, merubah isi input.css, menambah fitur smooth scrolling dan back to top serta fiturnya juga menambahkah fitur dark mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // hamburger
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const toTop = document.querySelector("#toTop");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
@@ -14,7 +15,44 @@ window.onscroll = function () {
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
+
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark Moode
+const darkTogle = document.querySelector("#darkmode");
+const html = document.querySelector("html");
+
+darkTogle.addEventListener("click", function () {
+  if (darkTogle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindah posisi togle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkTogle.checked = true;
+} else {
+  darkTogle.checked = false;
+}
